482210701172bc30d4968a0b5d9dc042ae65a6cf
Ravi Tripathi <42661853+dreamgoogle@users.noreply.github.com>
2020-05-24 18:27:58 +0530

    Improvements to still filter rslts of all_inputs.h
    This version was tested for 6m7_203 and 2 more softwares
    1) The previous version fetched data with non-standard data type also due to which all_inputs.h generated cannot be directly used for inclusion. This version also may be directly included as it contains functions with NVMY channel names in function prototypes. But since these cases are very less. Hence manageable
    2) This version also collects NC's first and declarations later.
    3) Next version may be aimed at removing function prototypes.
    2) However

--- input_data_from_pp_gui.py
+++ input_data_from_pp_gui.py
@@ -6,22 +6,46 @@ import tkinter as Collect_data_TK
 from tkinter import font
 root = Collect_data_TK.Tk()
 decl = "extern"
+functions = "extern void"
 nc = "#define\tN"
+datatypes = ['boolean','u8','s8','u16','s16','u32','s32','u64','s64','flag','f32','f64','unsigned','signed','uint32','sint32','float','uint16','sint16','uint8','sint8','uint64','sint64','int','char']
 def gen_file_api(entry):
-	try:
-		for root, dirs, files in os.walk(entry):
-			with open(os.path.join(root,'all_inputs.h'), "w") as wfh_var:
-				for file in files:
-					if file.endswith('.h') and (not file.endswith('_mcr.h')) and (not file.endswith('all_inputs.h')):
-						with open(os.path.join(root,file), "r") as rfh:
-							s = " "
-							while(s):
-								s = rfh.readline()
-								if s.startswith(decl) or s.startswith(nc):
-									wfh_var.write(s)
-			lo_label['text'] = "all_inputs.h created at \n" + root
-	except:
-		lo_label['text'] = "Path Incorrect !! Please check."
+    try:
+        for root, dirs, files in os.walk(entry):
+            with open(os.path.join(root,'tmp_inp.h'), "w") as wfh_tmp:
+                for file in files:
+                    if file.endswith('.h') and (not file.endswith('_mcr.h')) and (not file.endswith('tmp_inp.h')) and (not file.endswith('all_inputs.h')):
+                        with open(os.path.join(root,file), "r") as rfh:
+                            s = " "
+                            while(s):
+                                s = rfh.readline()
+                                if s.startswith(nc):
+                                    wfh_tmp.write(s)
+                                elif s.startswith(decl) and (not s.startswith(functions)):
+                                    L = s.split()
+                                    size = len(L)
+                                    if (size > 2):
+                                        if (L[1] in datatypes or L[2] in datatypes):
+                                            wfh_tmp.write(s)
+                                    else:  
+                                        if (L[1] in datatypes):
+                                            wfh_tmp.write(s)
+            with open(os.path.join(root,'tmp_inp.h'), "r") as rfh_var:
+                with open(os.path.join(root,'all_inputs.h'), "w+") as wfh_var:
+                    s = " "
+                    while(s):
+                        s = rfh_var.readline()
+                        if s.startswith(nc):
+                            wfh_var.write(s)
+                    rfh_var.seek(0)
+                    s = " "
+                    while(s):
+                        s = rfh_var.readline()
+                        if s.startswith(decl):
+                            wfh_var.write(s)
+            lo_label['text'] = "all_inputs.h created at \n" + root
+    except:
+        lo_label['text'] = "Path Incorrect !! Please check."
 
 canvas = Collect_data_TK.Canvas(root, height=HEIGHT, width=WIDTH )
 canvas.pack()
